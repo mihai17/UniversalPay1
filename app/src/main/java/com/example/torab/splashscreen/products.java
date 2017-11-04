3c28a1afd5ee1e3f7cb9a741655340fbe3373ba6
mihai17 <mihailcraciun1@gmail.com>
2017-11-04 23:02:24 +0200

    cola

---
+++ app/src/main/java/com/example/torab/splashscreen/products.java
@@ -0,0 +1,18 @@
+package com.example.torab.splashscreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by petre on 11/4/2017.
+ */
+
+public class products  extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.products);
+    }
+}
+
--- app/src/main/java/com/example/torab/splashscreen/vending_machines.java
+++ app/src/main/java/com/example/torab/splashscreen/vending_machines.java
@@ -1,17 +1,33 @@
 package com.example.torab.splashscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by petre on 11/4/2017.
  */
 
 public class vending_machines extends AppCompatActivity {
+    private static final String TAG = "vending_machines";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vending_machines);
+        Log.d(TAG, "onCreate: Starting");
+
+        Button button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Clicked button8");
+                Intent intent = new Intent(vending_machines.this,products.class);
+                startActivity(intent);
+            }
+        });
     }
 }
